6c83e45128fcb4720ba9d3f21fd81afbb6e23319
vinodh-balasubramanian <vinodh.balasubramanian@gmail.com>
2025-09-16 12:56:46 +0200

    test comment

--- src/main/java/com/labforward/wordfrequency/WordFrequencyApplication.java
+++ src/main/java/com/labforward/wordfrequency/WordFrequencyApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WordFrequencyApplication {
-
+    //base
 	public static void main(String[] args) {
 		SpringApplication.run(WordFrequencyApplication.class, args);
 	}
